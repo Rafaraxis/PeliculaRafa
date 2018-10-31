9135fa077643b5847ea8f504c57ec9d5ee0683e4
Alumno <ulsa@iMacD105.local>
2018-10-31 09:51:42 -0700

    hola

---
+++ peliculasrafa/Datos.swift
@@ -0,0 +1,14 @@
+//
+//  Datos.swift
+//  peliculasrafa
+//
+//  Created by Alumno on 31/10/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Datos {
+    static var resultadosPeliculas : [Pelicula] = [Pelicula(titulo: "Space Jam", año: 1996), Pelicula(titulo: "Aladdin", año: 1992),]
+}
--- peliculasrafa/ViewController.swift
+++ peliculasrafa/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import Alamofire
+// Heredar los protocolos
+//Data source y Delegate
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 94.5
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Datos.resultadosPeliculas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "cellResultadoPelicula") as! CeldaPeliculaController
+        celda.lblTitulo.text = Datos.resultadosPeliculas[indexPath.row].titulo
+        celda.lblAño.text = "\(Datos.resultadosPeliculas[indexPath.row].año)"
+        return celda
+    }
+    
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var aiCargando: UIActivityIndicatorView!
+    @IBOutlet weak var txtBusqueda: UITextField!
+    @IBOutlet weak var tvResultadosPeliculas: UITableView!
+    @IBAction func doTapBuscarPelicula(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Alamofire.request("https://www.omdbapi.com/?apikey=4799d46a&s=godfather").responseJSON {
+            response in
+            
+            Datos.resultadosPeliculas.removeAll()
+            
+            if let dictResponse = response.result.value as? NSDictionary {
+                if let arrResultados = dictResponse.value(forKey: "Search") as? NSArray {
+                    for resultado in arrResultados {
+                        if let dictResultado = resultado as?
+                            NSDictionary {
+                            let nuevoResultado = Pelicula(diccionario: dictResultado)
+                            
+                            Datos.resultadosPeliculas.append(nuevoResultado)
+                        }
+                }
+                    self.tvResultadosPeliculas.reloadData()
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+        func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    }
 }
 
